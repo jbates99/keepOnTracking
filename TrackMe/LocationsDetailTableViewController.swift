dc7cfb3786f1e38325573dfc5d27e63549b1912a
Joshua Bates <batesjoshua50@gmail.com>
2016-07-07 16:00:01 -0600

    refactored with new persistence

--- TrackMe/LocationsDetailTableViewController.swift
+++ TrackMe/LocationsDetailTableViewController.swift
@@ -56,16 +56,6 @@ class LocationsDetailTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
@@ -82,43 +72,11 @@ class LocationsDetailTableViewController: UITableViewController {
         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     }
     
-    
-    
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension LocationsDetailTableViewController: ButtonTableViewCellDelegate {
     
     func buttonCellButtonTapped(sender: ButtonTableViewCell) {
-        if let indexPath = tableView.indexPathForCell(sender) {
-            let location = RegionController.sharedController.geoFences[indexPath.row]
-            RegionController.statusToggled(location)
-            tableView.reloadData()
-        }
+       
     }
 }
--- TrackMe/RegionController.swift
+++ TrackMe/RegionController.swift
@@ -13,55 +13,13 @@ import CloudKit
 
 class RegionController {
     
-    static let sharedController = RegionController()
-    
-    var geoFences = [GeoFence]()
-    
     static func createRegion(coordinate: CLLocationCoordinate2D, radius: CLLocationDistance, name: String, status: Bool = true) {
-        let region = CLCircularRegion(center: coordinate, radius: radius, identifier: name)
-        let geofence = GeoFence.init(region: region, status: status, name: name)
-        
-        // MARK: Creates Record
-        let record = CKRecord(geoFence: geofence)
-        let db = CKContainer.defaultContainer().privateCloudDatabase
-        db.saveRecord(record) { (record, error) in
-            if let error = error {
-                print("Error saving \(geofence) to CloudKit: \(error)")
-                return
-            }
-            dispatch_async(dispatch_get_main_queue()) {
-                self.sharedController.geoFences.append(geofence)
-            }
-        
-        }
-        
         // MARK: Starts Monitoring Region
+        let region = CLCircularRegion(center: coordinate, radius: radius, identifier: name)
         guard let delegate = (UIApplication.sharedApplication().delegate as? AppDelegate) else { return }
         delegate.setUpRegions()
-        if let manager = (UIApplication.sharedApplication().delegate as? AppDelegate)?.manager {
-            manager.startMonitoringForRegion(region)
-        } else {
-            print("Unable to access the manager")
-        }
-    }
-    
-    static func statusToggled(geoFence: GeoFence) {
-        if geoFence.status == false {
-            // geoFence.status = true
-            if let manager = (UIApplication.sharedApplication().delegate as? AppDelegate)?.manager {
-                manager.startMonitoringForRegion(geoFence.region)
-            } else {
-                print("Unable to access the manager")
-            }
-        } else {
-            // geoFence.status = false
-            if let manager = (UIApplication.sharedApplication().delegate as? AppDelegate)?.manager {
-                manager.stopMonitoringForRegion(geoFence.region)
-            } else {
-                print("Unable to access the manager")
-            }
-        }
-        
+        let manager = delegate.manager
+        manager.startMonitoringForRegion(region)
     }
     
 }
